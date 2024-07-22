63cdd2fa62c0c5390e8902b3b724d864f14c54f1
Zayed <zaidshamlan9@gmail.com>
2024-07-23 00:18:57 +0300

    new update

--- lib/features/home/ui/widgets/doctors_list/doctors_listview_item.dart
+++ lib/features/home/ui/widgets/doctors_list/doctors_listview_item.dart
@@ -1,8 +1,11 @@
 import 'package:appointment/features/home/data/moduls/specializations_response_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../../../../../core/helpers/spacing.dart';
+import '../../../../../core/theming/colors.dart';
 import '../../../../../core/theming/styles.dart';
 
 
@@ -17,13 +20,35 @@ class DoctorsListViewItem extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 12.h),
       child: Row(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12.r),
-            child: Image.asset(
-              'assets/images/Doctors.png',
+         CachedNetworkImage(
+            imageUrl:
+                "https://static.wikia.nocookie.net/five-world-war/images/6/64/Hisoka.jpg/revision/latest?cb=20190313114050",
+            progressIndicatorBuilder: (context, url, downloadProgress) {
+              return Shimmer.fromColors(
+                baseColor: ColorsManger.lightGrey,
+                highlightColor: Colors.white,
+                child: Container(
+                  width: 110.w,
+                  height: 120.h,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            imageBuilder: (context, imageProvider) => Container(
               width: 110.w,
               height: 120.h,
-              fit: BoxFit.cover,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12.0),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
           horizontalSpace(16),
